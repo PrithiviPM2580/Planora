a7d18da52cd7b817ed0a7678f9fbec11c009562f
Prithivi <prithivi@users.noreply.github.com>
2026-02-16 00:40:34 +0545

    feat: add createVerification function to handle verification data storage

--- backend/src/repositories/auth.repository.ts
+++ backend/src/repositories/auth.repository.ts
@@ -1,4 +1,6 @@
 import User from "@/models/user.model.js";
+import type { Types } from "mongoose";
+import Verification from "@/models/verification.model.js";
 
 export const findUserByEmail = async (email: string) => {
   return User.findOne({ email });
@@ -10,4 +12,12 @@ export const createUser = async (userData: {
   password: string;
 }) => {
   return User.create(userData);
+};
+
+export const createVerification = async (verificationData: {
+  userId: Types.ObjectId;
+  token: string;
+  expiresAt: Date;
+}) => {
+  return Verification.create(verificationData);
 };
