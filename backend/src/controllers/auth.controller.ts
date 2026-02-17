47aa7a4f2f22d8cdeef7ef11ac7dcff3d8c57ff1
Prithivi <prithivi@users.noreply.github.com>
2026-02-17 10:56:57 +0545

    feat: implement login and email verification controllers with success response handling

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,7 +1,11 @@
 import logger from "@/lib/logger.lib.js";
 import { successResponse } from "@/utils/success-response.util.js";
 import type { Request, Response } from "express";
-import { registerService } from "@/services/auth.service.js";
+import {
+  registerService,
+  loginService,
+  verifyEmailService,
+} from "@/services/auth.service.js";
 
 export const registerController = async (req: Request, res: Response) => {
   const { user } = await registerService(req.body);
@@ -16,5 +20,30 @@ export const registerController = async (req: Request, res: Response) => {
     201,
     "User registered successfully, Verification email sent please check and verify your account",
     { user },
+  );
+};
+
+export const loginController = async (req: Request, res: Response) => {
+  const { user, token } = await loginService(req.body);
+
+  logger.info("User logged in successfully", {
+    label: "Login_Controller",
+    userId: user.id,
+  });
+
+  successResponse(res, 200, "User logged in successfully", { user, token });
+};
+
+export const verifyEmailController = async (req: Request, res: Response) => {
+  await verifyEmailService(req.body);
+
+  logger.info("Email verified successfully", {
+    label: "Verify_Email_Controller",
+  });
+
+  successResponse(
+    res,
+    200,
+    "Email verified successfully, you can now login to your account",
   );
 };
